14ed88289111577ffb5a47461e3296944eb22fbe
Robert Forrest <robertforrest@live.com>
2022-03-28 15:44:40 +0100

    Bug fix: Corrected Z range

--- elementary/periodictable.py
+++ elementary/periodictable.py
@@ -6,7 +6,7 @@ class PeriodicTable():
     def __init__(self):
         self.elements = []
 
-        for Z in range(1,118):
+        for Z in range(1,119):
             self.elements.append(Element(Z))
 
         self.data = pd.DataFrame([element.__dict__ for element in self.elements ])
