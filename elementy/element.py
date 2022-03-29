6e37154294cd110b52b9d1f17313fd04d9defc5a
Robert Forrest <robertforrest@live.com>
2022-03-29 15:00:09 +0100

    Added mulliken electronegativity calculation

--- elementy/element.py
+++ elementy/element.py
@@ -1,5 +1,10 @@
+import elementy.mulliken as mulliken
+
 
 class Element():
 
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
+
+        self.electronegativity_mulliken = \
+            mulliken.calculate_mulliken_electronegativity(self)
