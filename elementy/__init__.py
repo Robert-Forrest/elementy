0bd26d03e7953f9fef9be6758af721df079a06da
Robert Forrest <robertforrest@live.com>
2022-03-29 10:24:39 +0100

    Expose elements list, added get_data function

--- elementy/__init__.py
+++ elementy/__init__.py
@@ -1,4 +1,16 @@
 from .periodictable import PeriodicTable
 
 PERIODIC_TABLE = PeriodicTable()
-data = PERIODIC_TABLE.data
+elements = PERIODIC_TABLE.elements
+dataframe = PERIODIC_TABLE.dataframe
+
+
+def get_data(feature_name, elements=None):
+
+    if elements is not None:
+        selected_data = dataframe.loc[dataframe['symbol'].isin(elements)][[
+            "symbol", feature_name]]
+    else:
+        selected_data = dataframe[["symbol", feature_name]]
+
+    return selected_data
