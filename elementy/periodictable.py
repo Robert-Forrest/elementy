71a6fdd638485901fdc175aeaba2a3c3157f21c7
Robert Forrest <robertforrest@live.com>
2022-03-29 19:06:49 +0100

    Adjusted data fetching defaults

--- elementy/periodictable.py
+++ elementy/periodictable.py
@@ -10,12 +10,17 @@ class PeriodicTable():
         self.dataframe = pd.DataFrame(
             [element.__dict__ for element in self.elements])
 
-    def get_data(self, feature_name, elements=None):
+    def get_data(self, feature_names=None, elements=None):
 
         if elements is not None:
             selected_data = self.dataframe.loc[self.dataframe['symbol'].isin(
-                elements)][["symbol", feature_name]]
+                elements)]
         else:
-            selected_data = self.dataframe[["symbol", feature_name]]
+            selected_data = self.dataframe.copy()
+
+        if feature_names is not None:
+            if not isinstance(feature_names, list):
+                feature_names = [feature_names]
+            selected_data = selected_data[["symbol", *feature_names]]
 
         return selected_data
