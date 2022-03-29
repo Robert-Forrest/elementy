0d3a2f8cf7eba349b8835a3596824465a7d65208
Robert Forrest <robertforrest@live.com>
2022-03-29 08:29:41 +0100

    Test more elements, test atomic number

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -4,3 +4,12 @@ import elementy.elements as elements
 def test_element_symbol():
 
     assert elements.Hydrogen.symbol == "H"
+    assert elements.Iron.symbol == "Fe"
+    assert elements.Uranium.symbol == "U"
+
+
+def test_element_atomic_number():
+
+    assert elements.Hydrogen.atomic_number == 1
+    assert elements.Iron.atomic_number == 26
+    assert elements.Uranium.atomic_number == 92
