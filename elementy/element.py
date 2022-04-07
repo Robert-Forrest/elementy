f680b0efb734fd00adf341add8ed1a4129cb47c1
Robert Forrest <robertforrest@live.com>
2022-04-07 19:11:50 +0100

    Converted Element and PeriodicTable to dataclass

--- elementy/element.py
+++ elementy/element.py
@@ -1,16 +1,82 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
 import elementy.mulliken as mulliken
 import elementy.radii as radii
 
 
-class Element():
+@dataclass()
+class Element:
+    name: str
+    symbol: str
+    atomic_number: int
+    electrons: int
+    protons: int
+    neutrons: int
+    valence_electrons: int
+    group: int
+    period: int
+    block: str
+    series: str
+    orbitals: dict
+    periodic_number: Optional[int] = field(default=None)
+    radius_empirical: Optional[float] = field(
+        default=None, metadata={'unit': 'angstrom'})
+    radius_calculated: Optional[float] = field(
+        default=None, metadata={'unit': 'angstrom'})
+    radius_vanDerWaals: Optional[float] = field(
+        default=None, metadata={'unit': 'angstrom'})
+    radius_covalent: Optional[float] = field(
+        default=None, metadata={'unit': 'angstrom'})
+    radius_metallic: Optional[float] = field(
+        default=None, metadata={'unit': 'angstrom'})
+    radius_USE: Optional[float] = field(
+        default=None, metadata={'unit': 'angstrom'})
+    radius: float = field(init=False, metadata={'unit': 'angstrom'})
+    atomic_volume: float = field(
+        init=False, metadata={'unit': 'cubic angstrom'})
+    volume_miedema: Optional[float] = field(default=None)
+    mass: Optional[float] = field(default=None, metadata={
+                                  'unit': 'atomic mass units'})
+    valence: Optional[int] = field(default=None)
+    electron_affinity: Optional[float] = field(default=None)
+    wigner_seitz_electron_density: Optional[float] = field(default=None)
+    chemical_scale: Optional[float] = field(default=None)
+    mendeleev_universal_sequence: Optional[int] = field(default=None)
+    mendeleev_pettifor: Optional[int] = field(default=None)
+    mendeleev_modified: Optional[int] = field(default=None)
+    work_function: Optional[float] = field(default=None)
+    electronegativity_pauling: Optional[float] = field(default=None)
+    electronegativity_allen: Optional[float] = field(default=None)
+    electronegativity_miedema: Optional[float] = field(default=None)
+    electronegativity_mulliken: float = field(init=False)
+    miedema_R: Optional[float] = field(default=None)
+    ionisation_energies: Optional[list] = field(
+        default=None, metadata={'unit': 'electron-volts'})
+    chemical_hardness: Optional[float] = field(default=None)
+    chemical_potential: Optional[float] = field(default=None)
+    melting_temperature: Optional[float] = field(
+        default=None, metadata={'unit': 'kelvin'})
+    boiling_temperature: Optional[float] = field(
+        default=None, metadata={'unit': 'kelvin'})
+    fusion_enthalpy: Optional[float] = field(default=None)
+    vaporisation_enthalpy: Optional[float] = field(default=None)
+    molar_heat_capacity: Optional[float] = field(default=None)
+    molar_volume: float = field(init=False)
+    structure: Optional[str] = field(default=None)
+    thermal_conductivity: Optional[float] = field(default=None)
+    thermal_expansion: Optional[float] = field(default=None)
+    density: Optional[float] = field(
+        default=None, metadata={'unit': 'grammes per cubic centimetre'})
+    cohesive_energy: Optional[float] = field(default=None)
+    debye_temperature: Optional[float] = field(
+        default=None, metadata={'unit': 'kelvin'})
+    price: Optional[float] = field(default=None, metadata={'unit': 'dollars'})
 
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
-
-        self.electronegativity_mulliken = \
-            mulliken.calculate_mulliken_electronegativity(self)
+    def __post_init__(self):
 
         self.radius = radii.calculate_radius(self)
         self.atomic_volume = radii.calculate_atomic_volume(self)
-
         self.molar_volume = self.mass / self.density
+        self.electronegativity_mulliken = mulliken.\
+            calculate_mulliken_electronegativity(self)
--- elementy/periodictable.py
+++ elementy/periodictable.py
@@ -1,30 +1,18 @@
-import pandas as pd
+from dataclasses import dataclass, field
+from typing import Dict
 
 import elementy.elements as elements
+from .element import Element
 
 
-class PeriodicTable():
-    def __init__(self):
-        self.elements = elements.get_elements()
+def make_periodic_table():
+    periodic_table_elements = {}
+    for element in elements.get_elements():
+        periodic_table_elements[element.symbol] = element
+    return periodic_table_elements
 
-        self.dataframe = pd.DataFrame(
-            [element.__dict__ for element in self.elements])
-        self.dataframe = self.dataframe.set_index(['symbol'])
 
-        self.data = {}
-        for element in self.elements:
-            self.data[element.symbol] = element.__dict__
-
-    def get_data(self, feature_names=None, elements=None):
-
-        if elements is not None:
-            selected_data = self.dataframe.loc[elements]
-        else:
-            selected_data = self.dataframe
-
-        if feature_names is not None:
-            if not isinstance(feature_names, list):
-                feature_names = [feature_names]
-            selected_data = selected_data[["symbol", *feature_names]]
-
-        return selected_data
+@dataclass
+class PeriodicTable:
+    elements: Dict[str, Element] = field(
+        default_factory=make_periodic_table)
